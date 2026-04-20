3dfba65923585c36e7412e464b66193900191646
Deepakj-2023 <deepakj.aids2023@citchennai.net>
2026-04-20 15:32:07 +0530

    Search Tool Implemented

--- tools/search_doc_tool.py
+++ tools/search_doc_tool.py
@@ -1,2 +1,15 @@
-def search_docs():
-    pass
+from utils.vector_store import VectorStore
+
+# Initialize once (shared instance)
+store = VectorStore()
+
+def search_documents_tool(query: str) -> dict:
+    """
+    Tool for LLM agent
+    """
+    results = store.search(query)
+
+    return {
+        "query": query,
+        "results": results
+    }
